21834ddba356d13df2832b86a4390a985e07d59d
Tharisuudev <thalyssouzadev@gmail.com>
2023-08-12 14:57:29 -0400

    Fixing Bug test1

--- backend/src/interfaces/IRepositories.ts
+++
@@ -1,5 +0,0 @@
-import { IContactDTO } from "./ContactDTO";
-
-export interface IContactRepository {
-  save(data: IContactDTO): Promise<void>;
-}
--- backend/src/interfaces/MailDTO.ts
+++
@@ -1,7 +0,0 @@
-export interface IMailDTO {
-  from: string;
-  subject: string;
-  content: string;
-  number: string;
-  name: string;
-}
